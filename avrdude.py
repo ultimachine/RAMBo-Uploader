2cc5763f12f123144feac0cc9e0fc64ff50ea548
Steve Kelly <sjkelly@wpi.edu>
2013-06-18 14:15:33 -0500

    add params to avrdude

--- avrdude.py
+++ avrdude.py
@@ -11,13 +11,22 @@ class avrdude():
 		self.programmer = ""
 		self.programmerSN = ""
 		self.port = ""
+		self.baudrate = ""
+		self.configFile = ""
+		self.autoEraseFlash = ""
 	def upload(self, target):
 		#call avrdude as a subprocess
 		cmd = self.path + " -c " + self.programmer + " -P " + self.port + ":" +self.programmerSN
 		if target.name:
 			cmd += " -p " + target.name
+		if self.baudrate:
+			cmd += " -b " + self.baudrate
+		if self.configFile:
+			cmd += " -C " + self.configFile
+		if self.autoEraseFlash:
+			cmd += " -D"
 		if target.bootloader:
-			cmd += " -U " + target.bootloader
+			cmd += " -Uflash:w:" + target.bootloader + ":i"
 		if target.extFuse:
 			cmd += " -Uefuse:w:" + target.extFuse + ":m"
 		if target.highFuse:
