a50d672c3f6099e4af0009fd11186230eabab664
UltiMachine Developers <info@ultimachine.com>
2013-06-28 01:07:08 -0500

    Hash out test sequence

--- test_process.py
+++ test_process.py
@@ -3,10 +3,13 @@
 from serial import Serial, SerialException
 from time import gmtime, strftime
 import os
+import sys
 import traceback
 import time 
 import string
-
+import threading 
+import subprocess 
+import re
 try:
 	controller = Serial(port = "/dev/ttyACM0", baudrate = 115200)
 	target = Serial(port = None, baudrate = 115200)
@@ -23,19 +26,29 @@ stepperSpeed = 100
 testing = True
 state = "start"
 entered = False
+keyboard = ""
 output = ""
 targetOut = ""
 refs = []
+fullstepTest = []
+halfstepTest = []
+quarterstepTest = []
+sixteenthstepTest = []
+vrefTest = []
+mosfethighTest = []
+mosfetlowTest = []
+thermistorTest = []
+
+groupn = lambda l, n: zip(*(iter(l),) * n)
+
 print "Monitoring test controller..."
 
 while(testing):
 	output += controller.read(controller.inWaiting())
-	if(target.port): targetOut += target.read(target.inWaiting())
+	if(target.port): 
+		targetOut += target.read(target.inWaiting())
 	if state == "start":
 		if "start" in output:
-			target.port = "/dev/ttyACM2"
-			target.open()
-			print "Target port : " + target.port
 			state = "homing"
 			print "Test started at " + strftime("%Y-%m-%d %H:%M:%S", gmtime())
 			output = ""
@@ -53,36 +66,77 @@ while(testing):
 	elif state == "clamping":
 		if not entered:
 			print "Clamping board..."
-			controller.write("C18550F3000U_")
+			controller.write("C18650F3000U_")
 			entered = True
 		if "ok" in output:
 			state = "powering"
 			entered = False
 			print "Board Clamped."
 			output = ""
+	elif state == "uploading":
+		print "Uploading Bootloader and setting fuses..."
+		avr32u2 = subprocess.Popen(['/usr/bin/avrdude', '-v', '-v', '-c', u'avrispmkII', '-P', u'usb:0200158420', u'-patmega32u2', u'-Uflash:w:/home/ultimachine/workspace/RAMBo/bootloaders/RAMBo-usbserial-DFU-combined-32u2.HEX:i', u'-Uefuse:w:0xF4:m', u'-Uhfuse:w:0xD9:m', u'-Ulfuse:w:0xEF:m', u'-Ulock:w:0x0F:m'])
+		avr32u2State = avr32u2.wait()
+		print "atmega 32U2: "
+		if avr32u2State == 0:
+			print "Uploaded!"
+			state = "connecting target"
+		else:
+			print "Upload Failed"
+			state = "board fail"
+		avr2560 = subprocess.Popen(['/usr/bin/avrdude', '-v', '-v', '-c', u'avrispmkII', '-P', u'usb:0200158597', u'-pm2560', u'-Uflash:w:/home/ultimachine/workspace/RAMBo/bootloaders/stk500boot_v2_mega2560.hex:i', u'-Uefuse:w:0xFD:m', u'-Uhfuse:w:0xD0:m', u'-Ulfuse:w:0xFF:m', u'-Ulock:w:0x0F:m'])
+		avr560State = avr2560.wait()
+		print "atmega 2560: "
+		if avr2560State == 0:
+			print "Uploaded!"
+			state = "connecting target"
+		else:
+			print "Upload Failed"
+			state = "board fail"
+			entered = False
+	elif state == "program for test":
+		print "Programming for the tests..."
+		prog = subprocess.Popen("avrdude -patmega2560 -cstk500v2 -P/dev/ttyACM1 -b115200 -D -Uflash:w:/home/ultimachine/workspace/Test_Jig_Firmware/target_test_firmware.hex".split())
+		state = prog.wait()
+		if state == 0:
+			print "Finished upload. Waiting for connection..."
+			state = "connecting target"
+			time.sleep(3)
+		else:
+			print "Upload failed"
+			state = "board fail"
+			entered = False
+	elif state == "connecting target":
+		print "Attempting connect..."	
+		target.port = "/dev/ttyACM1"
+		target.open()
+		print "Target port : " + target.port 	
+		state = "fullstep"	
 	elif state == "powering":
 		if not entered:
 			print "Powering Board..."
 			controller.write("W3H_")
 			entered = True
 		if "ok" in output:
-			state = "fullstep"
+			state = "program for test"
 			entered = False
 			print "Target Board powered."
 			output = ""
+			
 	elif state == "fullstep":
 		if not entered:
 			entered = True
-			controller.write("M"+str(monitorTime)+"F100_")
 			print "Testing steppers at full step..."
 			target.write("U1_")
+			controller.write("M"+str(monitorTime)+"F100_")
 			target.write("C200F800U")
 			target.write("C200F800D")
 		if "ok" in output:
 			state = "halfstep"
 			entered = False
 			print "Full Step test finished."
-			print output
+			fullstepTest =groupn(map(int,re.findall(r'\b\d+\b', output)),10)
+			print fullstepTest
 			output = ""
 	elif state == "halfstep":
 		if not entered:
@@ -96,7 +150,8 @@ while(testing):
 			state = "quarterstep"
 			entered = False
 			print "Half Step test finished."
-			print output
+			halfstepTest = groupn(map(int,re.findall(r'\b\d+\b', output)),10)
+			print halfstepTest
 			output = ""
 	elif state == "quarterstep":
 		if not entered:
@@ -110,7 +165,8 @@ while(testing):
 			state = "sixteenthstep"
 			entered = False
 			print "Quarter Step test finished."
-			print output
+			quarterstepTest = groupn(map(int,re.findall(r'\b\d+\b', output)),10)
+			print quarterstepTest
 			output = ""
 	elif state == "sixteenthstep":
 		if not entered:
@@ -124,7 +180,8 @@ while(testing):
 			state = "vrefs"
 			entered = False
 			print "Sixteenth Step test finished."
-			print output
+			sixteenthTest = groupn(map(int,re.findall(r'\b\d+\b', output)),10)
+			print sixteenthstepTest
 			output = ""
 	elif state == "vrefs":
 		if not entered:
@@ -136,51 +193,59 @@ while(testing):
 			controller.write("A3_") #e1
 			print "Testing stepper driver references..."
 		if output.count("ok") == 5:
-			state = "finished"
+			state = "mosfet high"
 			entered = False
 			print "Vrefs acquired"
-			print output 
+			vrefTest = map(int,re.findall(r'\b\d+\b', output)) 
+			print vrefTest	
 			output = ""
 			targetOut = ""
 	elif state == "mosfet high":
 		if not entered:
 			entered = True
-			print "Testing Mosfets..."
+			print "Testing Mosfets High..."
 			target.write("W9H")
+			controller.write("R44_")
 			target.write("W8H")
+			controller.write("R32_")
 			target.write("W7H")
+			controller.write("R45_")
 			target.write("W6H")
+			controller.write("R31_")
 			target.write("W3H")
+			controller.write("R46_")
 			target.write("W2H")
-		if targetOut.count("ok") == 6:
-			controller.write("R44_")
-			controller.write("R32_")
-			controller.write("R45_")
-			controller.write("R31_")
-			controller.write("R46_")
 			controller.write("R30_")
+		if output.count("ok") == 6:
 			targetOut = ""
-		if output.count("ok") >= 6:
-			target.write("W9L")
+			print "Mosfet outputs recorded."
+			mosfethighTest = map(int,re.findall(r'\b\d+\b', output)) 
+			state = "mosfet low"
+			print mosfethighTest
+			output = ""
+			targetOut = ""
+			entered = False
+	elif state == "mosfet low":
+		if not entered:
+			entered = True
+			print "Testing mosfets Low..."
+			controller.write("R44_")
 			target.write("W8L")
-			target.write("W7L")
-			target.write("W6L")
-			target.write("W3L")
-			target.write("W2L")
-			print output
-			output = ""
-		if targetOut.count("ok") >= 6:
-			controller.write("R44_")
 			controller.write("R32_")
+			target.write("W7L")
 			controller.write("R45_")
+			target.write("W6L")
 			controller.write("R31_")
+			target.write("W3L")
 			controller.write("R46_")
+			target.write("W2L")
 			controller.write("R30_")
-			targetOut = ""
 		if output.count("ok") >= 6:
-			print "Mosfets tested."
-			print output
+			print "Mosfet outputs recorded."
+			mosfetlowTest = map(int,re.findall(r'\b\d+\b', output)) 
+			print mosfetlowTest
 			output = ""
+			targetOut = ""
 			state = "thermistors"
 			entered = False
 	elif state == "thermistors":
@@ -192,16 +257,37 @@ while(testing):
 			target.write("A2_")
 		if targetOut.count("ok") == 3:
 			print "Readings acquired."
-			print targetOut
+			thermistorTest = map(int,re.findall(r'\b\d+\b', targetOut)) 
+			print thermistorTest
 			targetOut = ""
 			entered = False
+			state = "program marlin"
+	elif state == "program marlin":
+		print "Disconnecting target from test script..."
+		target.close()
+		target.port = None 
+		print "Programming Marlin..."
+		prog = subprocess.Popen("avrdude -patmega2560 -cstk500v2 -P/dev/ttyACM1 -b115200 -D -Uflash:w:/home/ultimachine/workspace/johnnyr/Marlinth2.hex".split())
+		state = prog.wait()
+		if state == 0:
+			print "Finished Marlin upload."
 			state = "finished"
+		else:
+			print "Upload failed"
+			state = "board fail"
+			entered = False
 	elif state == "finished":
 		print "Testing finished without errors."
 		print "Powering off target"
 		controller.write("W3L_")
 		print "Preparing Test Jig for next board.."
 		controller.write("H5000_")
+		state = "start"	
+	elif state == "board fail":
+		print "Board failed"
 		target.close()
 		target.port = None
+		controller.write("W3L")
+		controller.write("H5000_")
 		state = "start"
+		entered = False
