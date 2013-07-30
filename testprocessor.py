9d3f4896a1eec734f6f301fc0613e6357b83315c
UltiMachine Developers <info@ultimachine.com>
2013-07-30 14:07:56 -0500

    commit a typo, then repeat twice

--- testprocessor.py
+++ testprocessor.py
@@ -225,7 +225,7 @@ class TestProcessor():
                     mosfetHigh=self.mosfetHigh,
                     mosfetLow=self.mosfetLow,
                     endstopHigh=self.endstopHigh,
-                    endstop=Lowself.endstopLow,
+                    endstopLow=self.endstopLow,
                     thermistors=self.thermistors)
 
     def _wasTimedOut(self, vals):
