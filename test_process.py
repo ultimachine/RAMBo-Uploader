5a2cd88fbed7ca3f8f535039da8f9d6e1b016c10
UltiMachine Developers <info@ultimachine.com>
2015-10-19 10:35:41 -0500

    slowed down clamping speed.

--- test_process.py
+++ test_process.py
@@ -44,7 +44,7 @@ monitorPin = 44 #PL5 on test controller
 triggerPin = 3 #bed on target board
 powerPin = 3 #bed on test controller
 homingRate = 11000 #5000
-clampingRate = 10000 #4000
+clampingRate = 8000 #4000
 # clamping length for : 1.1=18550, 1.2=16000
 #clampingLength = 18550
 #clampingLength = 16300
