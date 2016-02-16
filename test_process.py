e4bb87bc5b3c64fb30ea8765a2e331030b276c96
UltiMachine Developers <info@ultimachine.com>
2016-02-15 22:30:34 -0600

    change programmer type to wiring from stk500v2 for auto reset.

--- test_process.py
+++ test_process.py
@@ -176,7 +176,7 @@ vendorFirmware.bootloader = vendorFirmwarePath
 #Setup up avrdude config for upload to an Arduino.
 avrdude = Avrdude()
 avrdude.path = "/usr/bin/avrdude"
-avrdude.programmer = "stk500v2"
+avrdude.programmer = "wiring" #stk500v2
 avrdude.port = targetPort
 avrdude.baudrate = "115200"
 avrdude.autoEraseFlash = True
