e981a52cec8f89dc5a0601806a61ff3c3fa659d1
UltiMachine Developers <info@ultimachine.com>
2016-08-03 12:53:15 -0500

    undo last commit to shorten clamp distance

--- test_process.py
+++ test_process.py
@@ -62,7 +62,7 @@ clampingRate = 7000 #4000
 # clamping length for : 1.1=18550, 1.2=16000
 #clampingLength = 18550
 #clampingLength = 16300
-clampingLength = 15980 #16200
+clampingLength = 16000 #16200
 monitorFrequency = 1000
 stepperTestRPS = 5 #3 #rotations per second for the stepper test
 #controllerPort = "/dev/serial/by-id/usb-UltiMachine__ultimachine.com__RAMBo_64033353730351918201-if00"
