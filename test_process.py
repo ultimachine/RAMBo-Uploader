5f96ac688fe6f916ee9085a2781db6cc2141e2b5
UltiMachine Developers <info@ultimachine.com>
2018-12-28 12:30:19 -0600

    remove iserial checking before fw exists on board.

--- test_process.py
+++ test_process.py
@@ -633,41 +633,6 @@ while(testing):
 
         state = board.setState()
 
-        iserial = board.setISerial(targetPort)
-        if btldrState == True and iserial == 0  and len(sys.argv) >= 4:
-            state = "start"
-            continue
-
-        if iserial != 0:
-	    #Consistent iserial check: verify iserial matches first historical iserial number for the referenced serial number
-	    cursor.execute("""SELECT "tid","serial","iserial" FROM "public"."testdata" WHERE tid = (SELECT MIN(tid) FROM public.testdata WHERE "serial" = %s AND "iserial" IS NOT NULL)""", (serialNumber,) )
-	    rows = cursor.fetchall()
-	    if(len(rows)):
-	      print "historial: ", rows
-	      print "this 32u2 iserial: ", iserial
-	      if not iserial == str(rows[0][2]):
-	        print colored("Warning! This serial number was previously tested with a different 32u2 iserial. This board may have a duplicate serial number.",'yellow')
-                if checkingDuplicateSerials:
-	            state = "start"
-	            continue
-
-	    #Prevent extra serial numbers attached to single board by checking a boards first assigned serial number. 
-            #Example situation: retesting a board but scanning a fresh boards serial number. 
-            #lookup previous tests by iserial
-	    cursor.execute("""SELECT "tid","serial","iserial" FROM "public"."testdata" WHERE tid = (SELECT MIN(tid) FROM public.testdata WHERE "iserial" = %s)""", (iserial,) )
-	    rows = cursor.fetchall()
-	    if(len(rows)):
-	      print "historial: ", rows
-	      print "this 32u2 iserial: ", iserial
-	      if not serialNumber == str(rows[0][1]):
-	        print colored("Warning! This board was first tested with a different serial number then the one provided. Try again with the correct serial number.",'yellow')
-                if checkingDuplicateSerials:
-	            state = "start"
-	            continue
-
-        if iserial == 0:
-            iserial = None #prevent zero from getting stored in db as the iserial.
-
         print "Test started at " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
     elif state == "clamping":
